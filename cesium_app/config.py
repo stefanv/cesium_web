90320a64a929f1907dda607bcd7f38fac4780ddd
Stefan van der Walt <stefanv@berkeley.edu>
2017-04-26 11:00:16 -0700

    Ignore missing config files

--- cesium_app/config.py
+++ cesium_app/config.py
@@ -11,9 +11,10 @@ class Config(dict):
 
     def update_from(self, filename):
         """Update configuration from YAML file"""
-        more_cfg = yaml.load(open(filename))
-        dict.update(self, more_cfg)
-        print('[cesium] Loaded {}'.format(os.path.relpath(filename)))
+        if os.path.isfile(filename):
+            more_cfg = yaml.load(open(filename))
+            dict.update(self, more_cfg)
+            print('[cesium] Loaded {}'.format(os.path.relpath(filename)))
 
     def __getitem__(self, key):
         if ':' in key:
