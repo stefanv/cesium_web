a1bd6a7276333d0c99068287f99dcb7a3c031137
Brett Naul <brettnaul@gmail.com>
2017-06-12 13:57:28 +0200

    Removed unused app logic

--- baselayer/app/app_server.py
+++ baselayer/app/app_server.py
@@ -1,13 +1,5 @@
 import tornado.web
 
-import os
-import sys
-import pathlib
-
-from .config import Config
-from . import models
-from baselayer.app import model_util
-
 # This provides `login`, `complete`, and `disconnect` endpoints
 from social_tornado.routes import SOCIAL_AUTH_ROUTES
 
@@ -58,44 +50,3 @@ handlers = SOCIAL_AUTH_ROUTES + [
     (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
     (r'/(favicon.png)', tornado.web.StaticFileHandler, {'path': 'static/'})
 ]
-
-
-def make_app(config_files=None, debug=False):
-    """Create and return a `tornado.web.Application` object with specified
-    handlers and settings.
-
-    Parameters
-    ----------
-    config_files : list of str
-        Filenames of configuration files, loaded in the order specified.
-        By default, 'cesium.yaml.example' is used for defaults and 'cesium.yaml'
-        for further customizations.
-    debug : bool
-        Whether or not to start the app in debug mode.  In debug mode,
-        changed source files are immediately reloaded.
-
-    """
-    # baselayer settings
-    cfg = load_config(config_files)
-
-    if cfg['cookie_secret'] == 'abc01234':
-        print('!' * 80)
-        print('  Your server is insecure. Please update the secret string ')
-        print('  in the configuration file!')
-        print('!' * 80)
-
-    for path_name, path in cfg['paths'].items():
-        if not os.path.exists(path):
-            print("Creating %s" % path)
-            try:
-                os.makedirs(path)
-            except Exception as e:
-                print(e)
-
-    app = tornado.web.Application(handlers, **settings)
-    models.db.init(**cfg['database'])
-    model_util.create_tables()
-    model_util.create_tables(models.app_models)
-    app.cfg = cfg
-
-    return app
